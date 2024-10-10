8d35f9a62341d1d7e1200e598e537ca33305b00f
Asmaa Saad <80713230+Asmaa2796@users.noreply.github.com>
2024-10-11 01:10:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const jsonServer = require("json-server"); // importing json-server Library
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
+
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
