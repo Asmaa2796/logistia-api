83125223414eb0c04fb71ae6864b2b357213486a
Asmaa Saad <80713230+Asmaa2796@users.noreply.github.com>
2024-10-17 22:51:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const jsonServer = require('json-server');
+const path = require('path');
+const fs = require('fs');
+
 const server = jsonServer.create();
-const router = jsonServer.router({ services: [], blog: [], users: [] }); // Initialize with empty data
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // Use the static file
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
