7a0e8f07331043130f2bd7a632a5e92d13f3bcd7
Asmaa Saad <80713230+Asmaa2796@users.noreply.github.com>
2024-10-17 23:10:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json')); // Use the static file
+const data = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8');
+console.log('Data from db.json:', data); 
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
